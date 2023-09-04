8cf8ffff0460a756b64c626d34ff7f786d0bdb26
Artemides <edmpulasky@gmail.com>
2023-09-04 12:45:08 -0500

    fetch

---
+++ fundamentals/http.go
@@ -0,0 +1,25 @@
+package fundamentals
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func Fetch() {
+	for _, url := range os.Args[1:] {
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch error: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", b)
+	}
+}
