d18d969b8edafe18cefebc73f56a8121048c0d42
Artemides <edmpulasky@gmail.com>
2023-09-19 10:39:53 -0500

    interface values

--- fundamentals/oop/interfaces/satisfaction.go
+++ fundamentals/oop/interfaces/satisfaction.go
@@ -1,8 +1,11 @@
 package interfaces
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
 
@@ -12,5 +15,17 @@ func Flags() {
 	fmt.Printf("Sleeping for %v", *period)
 	time.Sleep(*period)
 	fmt.Println("Woke")
+
+}
+
+func RunInterfaces() {
+	var w io.Writer
+	fmt.Printf("w value %T\n", w)
+	w = os.Stdout
+	fmt.Printf("w value %T\n", w)
+	w = new(bytes.Buffer)
+	fmt.Printf("w value %T\n", w)
+	w = nil
+	fmt.Printf("w value %T\n", w)
 
 }
