d6b170a23b51b43e7d52684919e06739dbc0d0a5
Artemides <edmpulasky@gmail.com>
2023-10-02 15:50:19 -0500

    implement disk usage concurrently

--- fundamentals/concurrency/diskusage.go
+++ fundamentals/concurrency/diskusage.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -15,6 +16,25 @@ func walkdir(dir string, fileSize chan<- int64) {
 		if entry.IsDir() {
 			innerDir := filepath.Join(dir, entry.Name())
 			walkdir(innerDir, fileSize)
+			continue
+		}
+
+		fileInfo, err := entry.Info()
+		if err != nil {
+			fmt.Printf("reading %s , err: %s", entry.Name(), err)
+			continue
+		}
+		fileSize <- fileInfo.Size()
+	}
+}
+func walkdirV2(dir string, wg *sync.WaitGroup, fileSize chan<- int64, semaphore chan struct{}) {
+	defer wg.Done()
+	entries := direntriesV2(dir, semaphore)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			wg.Add(1)
+			innerDir := filepath.Join(dir, entry.Name())
+			go walkdirV2(innerDir, wg, fileSize, semaphore)
 			continue
 		}
 
@@ -36,6 +56,17 @@ func direntries(dir string) []fs.DirEntry {
 	return entries
 }
 
+func direntriesV2(dir string, semaphore chan struct{}) []fs.DirEntry {
+	semaphore <- struct{}{}
+	defer func() { <-semaphore }()
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+		return nil
+	}
+	return entries
+}
 func printDiskUsage(files, totalSize int64) {
 	fmt.Printf("%d files, %.1f GB\n ", files, float64(totalSize)/1e9)
 }
@@ -103,6 +134,57 @@ loop:
 
 	printDiskUsage(files, totalSize)
 }
+
+func DiskUsageV3() {
+	filesizes := make(chan int64)
+	semaphore := make(chan struct{}, 20)
+	var wg sync.WaitGroup
+
+	v := flag.Bool("v", false, "show progress message")
+	flag.Parse()
+	roots := flag.Args()
+
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	go func() {
+		for _, root := range roots {
+			wg.Add(1)
+			go walkdirV2(root, &wg, filesizes, semaphore)
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(filesizes)
+	}()
+
+	// set up
+
+	var tick <-chan time.Time
+
+	if *v {
+		tick = time.NewTicker(500 * time.Millisecond).C
+	}
+
+	var files, totalSize int64
+loop:
+	for {
+		select {
+		case size, ok := <-filesizes:
+			if !ok {
+				break loop
+			}
+			files++
+			totalSize += size
+		case <-tick:
+			printDiskUsage(files, totalSize)
+		}
+	}
+
+	printDiskUsage(files, totalSize)
+}
 func DiskUsageMain() {
-	DiskUsageV2()
+	DiskUsageV3()
 }
