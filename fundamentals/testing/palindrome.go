55ac0779c7582001e6fa67529e5a7656ced2ce32
Artemides <edmpulasky@gmail.com>
2023-10-08 20:10:17 -0500

    test palindrome

--- fundamentals/testing/palindrome.go
+++ fundamentals/testing/palindrome.go
@@ -1,5 +1,7 @@
 package testing
 
+import "unicode"
+
 func IsPalindrome(word string) bool {
 	for idx, _rune := range word {
 		if _rune != rune(word[len(word)-1-idx]) {
@@ -7,5 +9,23 @@ func IsPalindrome(word string) bool {
 		}
 	}
 
+	return true
+}
+
+func IsPalindromeV2(word string) bool {
+	var letters []rune
+	for _, _rune := range word {
+		if !unicode.IsLetter(_rune) {
+			continue
+		}
+
+		letters = append(letters, _rune)
+	}
+
+	for idx, _rune := range letters {
+		if _rune != letters[len(letters)-1-idx] {
+			return false
+		}
+	}
 	return true
 }
--- fundamentals/testing/palindrome_test.go
+++ fundamentals/testing/palindrome_test.go
@@ -9,10 +9,45 @@ func TestIsPalindrome(t *testing.T) {
 	if !IsPalindrome("kayak") {
 		t.Error(`IsPalindrome(kayak) = false`)
 	}
+
+	if !IsPalindrome("été") {
+		t.Errorf("IsPalindrome(%q) =f alse", "été")
+	}
+
+	input := "A man, a plan, a canal: Panama"
+	if !IsPalindrome(input) {
+		t.Errorf("IsPalindrome(%q) = false", input)
+	}
 }
 
 func TestNotPalindrome(t *testing.T) {
 	if IsPalindrome("palindrome") {
 		t.Error(`IsPalindrome(palindrome) = true`)
+	}
+}
+
+func TestIspalindromeV2(t *testing.T) {
+	testCases := []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false}, // non-palindrome
+		{"desserts", false},
+	}
+	for _, test := range testCases {
+		if got := IsPalindromeV2(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
+		}
 	}
 }
