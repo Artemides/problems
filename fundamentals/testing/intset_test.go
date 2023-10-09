aff3d7962d3a5a3f3a55bc12e31546ffe958912f
Artemides <edmpulasky@gmail.com>
2023-10-09 16:03:15 -0500

    test Add int set function

--- fundamentals/testing/intset_test.go
+++ fundamentals/testing/intset_test.go
@@ -8,43 +8,75 @@ import (
 )
 
 func TestIntset(t *testing.T) {
-	var set1 m.IntSet
-	var set2 m.IntSet
-
-	set1.AddAll(4, 5, 6, 3, 10, 11)
-	set2.AddAll(2, 6, 7, 5, 3, 2, 3)
 
 	testCases := []struct {
-		sets     []*m.IntSet
+		receiver *m.IntSet
+		params   interface{}
 		method   interface{}
 		expected interface{}
 	}{
 		{
-			sets:     []*m.IntSet{m.NewInset(1, 2), m.NewInset(3, 4)},
+			receiver: m.NewInset(1, 2),
+			params:   m.NewInset(3, 4),
 			method:   (*m.IntSet).Union,
 			expected: m.NewInset(1, 2, 3, 4),
+		},
+		{
+			receiver: m.NewInset(1, 2),
+			params:   112,
+			method:   (*m.IntSet).Add,
+			expected: m.NewInset(1, 2, 112),
 		},
 	}
 
 	for _, testCase := range testCases {
-		set1 := testCase.sets[0]
-		set2 := testCase.sets[1]
 
 		fn := reflect.TypeOf(testCase.method)
 		unionFn := (*m.IntSet).Union
+
 		if fn == reflect.TypeOf(unionFn) {
+			set1 := testCase.receiver
 			set1Str := set1.String()
+			set2, ok := testCase.params.(*m.IntSet)
+			if !ok {
+				t.Errorf(`invalid Union Params %T`, set2)
+				continue
+			}
 
 			unionFn(set1, set2)
 			expected, ok := testCase.expected.(*m.IntSet)
 			if !ok {
-				t.Errorf(``)
+				t.Errorf(`invalid expected result %T `, expected)
 				continue
 			}
 
 			if set1.String() != expected.String() {
 				t.Errorf(`%+v Union %+v = %+v ; expected: %+v `, set1Str, set2, set1.String(), expected.String())
 			}
+
+			continue
+		}
+		addFn := (*m.IntSet).Add
+		if fn == reflect.TypeOf(addFn) {
+			set1 := testCase.receiver
+			set1Str := set1.String()
+			param, ok := testCase.params.(int)
+			if !ok {
+				t.Errorf(`invalid Union Params %T`, param)
+				continue
+			}
+
+			addFn(set1, param)
+			expected, ok := testCase.expected.(*m.IntSet)
+			if !ok {
+				t.Errorf(`invalid expected result %T `, expected)
+				continue
+			}
+
+			if set1.String() != expected.String() {
+				t.Errorf(`%+v Add %d = %+v ; expected: %+v `, set1Str, param, set1.String(), expected.String())
+			}
+			continue
 		}
 	}
 
