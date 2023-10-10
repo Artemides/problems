792e9aac570a3939260703e5f4bee2267bd9b585
Artemides <edmpulasky@gmail.com>
2023-10-09 22:35:19 -0500

    add intersection  intset sets

--- fundamentals/testing/intset_test.go
+++ fundamentals/testing/intset_test.go
@@ -34,6 +34,12 @@ var intsetTestCases = []insetTestCase{
 		method:   (*m.IntSet).Has,
 		expected: true,
 	},
+	{
+		receiver: m.NewInset(12, 22),
+		params:   m.NewInset(12, 11, 23),
+		method:   (*m.IntSet).IntersectWith,
+		expected: m.NewInset(12),
+	},
 }
 
 func TestIntset(t *testing.T) {
@@ -42,24 +48,28 @@ func TestIntset(t *testing.T) {
 		fn := reflect.TypeOf(testCase.method)
 
 		if unionFn := (*m.IntSet).Union; fn == reflect.TypeOf(unionFn) {
-			insetUnion(unionFn, testCase, t)
+			union(unionFn, testCase, t)
 			continue
 		}
 
 		if addFn := (*m.IntSet).Add; fn == reflect.TypeOf(addFn) {
-			intsetAdd(addFn, testCase, t)
+			add(addFn, testCase, t)
 			continue
 		}
 
 		if hasFunc := (*m.IntSet).Has; fn == reflect.TypeOf(hasFunc) {
-			intsetHas(hasFunc, testCase, t)
+			has(hasFunc, testCase, t)
+			continue
+		}
+		if intersectWithFunc := (*m.IntSet).IntersectWith; fn == reflect.TypeOf(intersectWithFunc) {
+			intersects(intersectWithFunc, testCase, t)
 			continue
 		}
 	}
 
 }
 
-func insetUnion(unionFn func(*m.IntSet, *m.IntSet), testCase insetTestCase, t *testing.T) {
+func union(unionFn func(*m.IntSet, *m.IntSet), testCase insetTestCase, t *testing.T) {
 	set1 := testCase.receiver
 	set1Str := set1.String()
 	set2, ok := testCase.params.(*m.IntSet)
@@ -80,12 +90,12 @@ func insetUnion(unionFn func(*m.IntSet, *m.IntSet), testCase insetTestCase, t *t
 		return
 	}
 }
-func intsetAdd(addFn func(*m.IntSet, int), testCase insetTestCase, t *testing.T) {
+func add(addFn func(*m.IntSet, int), testCase insetTestCase, t *testing.T) {
 	set1 := testCase.receiver
 	set1Str := set1.String()
 	param, ok := testCase.params.(int)
 	if !ok {
-		t.Errorf(`invalid Union Params %T`, param)
+		t.Errorf(`invalid Add Params %T`, param)
 		return
 	}
 
@@ -100,11 +110,11 @@ func intsetAdd(addFn func(*m.IntSet, int), testCase insetTestCase, t *testing.T)
 		t.Errorf(`%+v Add %d = %+v ; expected: %+v `, set1Str, param, set1.String(), expected.String())
 	}
 }
-func intsetHas(hasFunc func(*m.IntSet, int) bool, testCase insetTestCase, t *testing.T) {
+func has(hasFunc func(*m.IntSet, int) bool, testCase insetTestCase, t *testing.T) {
 	set1 := testCase.receiver
 	param, ok := testCase.params.(int)
 	if !ok {
-		t.Errorf(`invalid Union Params %T`, param)
+		t.Errorf(`invalid Has Params %T`, param)
 		return
 	}
 
@@ -117,5 +127,25 @@ func intsetHas(hasFunc func(*m.IntSet, int) bool, testCase insetTestCase, t *tes
 
 	if has != expected {
 		t.Errorf(`%+v Has %v = %v; expected: %v`, set1.String(), param, has, expected)
+	}
+}
+func intersects(intersectWithFunc func(*m.IntSet, *m.IntSet), testCase insetTestCase, t *testing.T) {
+	set1 := testCase.receiver
+	set1Str := set1.String()
+	param, ok := testCase.params.(*m.IntSet)
+	if !ok {
+		t.Errorf(`invalid intersectWithFunc Params %T`, param)
+		return
+	}
+
+	intersectWithFunc(set1, param)
+	expected, ok := testCase.expected.(*m.IntSet)
+	if !ok {
+		t.Errorf(`invalid expected result %T `, expected)
+		return
+	}
+
+	if set1 != expected {
+		t.Errorf(`%+v Intersect %+v = %+v; expected: %+v`, set1Str, param.String(), set1.String(), expected.String())
 	}
 }
